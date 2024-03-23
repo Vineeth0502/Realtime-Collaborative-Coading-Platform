36c4ca3034355f024a36b0c932dbcf25081e2a9a
prasanthiveerla3 <157150609+prasanthiveerla3@users.noreply.github.com>
2024-03-22 23:20:08 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,77 +1,78 @@
-import React, { useState } from 'react'
-import {v4 as uuidV4} from 'uuid';
-import toast from 'react-hot-toast';
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [roomId,setRoomId] = useState('');
-  const [username,setUsername] = useState('');
-  const createNewRoom = (e) => {
-    e.preventDefault();
-    const id=uuidV4();
-    setRoomId(id);
-    toast.success('Created a new room');
-  };
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
 
-  const joinRoom = () => {
-    if(!roomId || !username){
-      toast.error('ROOM ID & username is required');
-      return;
-    }
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
 
-    //Redirect
-    navigate(`/editor/${roomId}`,{
-      state: {
-        username,
-      }
-    })
-  };
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
+        }
 
-  const handleInputEnter=(e)=>{
-     if(e.code==='Enter'){
-        joinRoom();
-     }
-  };
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
 
-  return(
-   <div className="homePageWrapper" style={{backgroundColor: "#070f2b"}}>
-    {/* <h2 className= "rubik-glitch" style={{position:"absolute",top:"0px",left:"120px", fontSize: "36px"}}>
-      Real  Time Collaborative Coding Editor</h2> */}
-    <img className='logo_contain' src='/pic.jpeg' style={{height: "400px", width: "600px", marginLeft: "105px",}}></img>
-    <div className="formWrapper" style={{backgroundColor: "lightsteelblue",height: "510px", marginRight: "50px"}}>
-        <img className="homePageLogo" src="/logo-icon.png" style={{marginTop: "90px"}}></img>
-        {/* <img className="homePageLogo" src="/codetogether.png"></img> */}
-        <h4 className="mainLabel">Paste invitaiton ROOM ID</h4>
-        <div className="inputGroup">
-          <input 
-          type="text"
-           className="inputBox" 
-           placeholder="ROOM ID"
-           onChange={(e)=>setRoomId(e.target.value)}
-           value={roomId}
-           onKeyUp={handleInputEnter}
-          />
-           <input 
-          type="text"
-           className="inputBox" 
-           placeholder="USER NAME"
-           onChange={(e)=>setUsername(e.target.value)}
-           value={username}
-           onKeyUp={handleInputEnter}
-          />
-          <buton className="btn joinBtn" onClick={joinRoom} style={{  width: "215px", marginLeft: "90px"}}>Join</buton>
-          <span className="createInfo">
-            If you don't have an invite then create &nbsp;
-            <a onClick={createNewRoom} href="">
-              new room
-            </a>
-          </span>
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinRoom();
+        }
+    };
+
+    return (
+        <div className="homePageWrapper" style={{ backgroundColor: '#070f2b' }}>
+            <Toaster />
+            <img className="logo_contain" src="/pic.jpeg" style={{ height: '400px', width: '600px', marginLeft: '105px' }}></img>
+            <div className="formWrapper" style={{ backgroundColor: 'lightsteelblue', height: '510px', marginRight: '50px' }}>
+                <img className="homePageLogo" src="/logo-icon.png" style={{ marginTop: '90px' }}></img>
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="USER NAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom} style={{ width: '215px', marginLeft: '90px' }}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                        If you don't have an invite then create &nbsp;
+                        <a onClick={createNewRoom} href="#">
+                            new room
+                        </a>
+                    </span>
+                </div>
+            </div>
         </div>
-    </div>
-  </div>
-  );
+    );
 };
 
 export default Home;
