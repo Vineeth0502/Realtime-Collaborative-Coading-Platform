95b45fdf55816bb38ae8bf91dcae4ed713dd2ad8
prasanthiveerla3 <157150609+prasanthiveerla3@users.noreply.github.com>
2024-03-22 23:16:55 -0500

    Update Client.js

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Avatar from 'react-avatar';
 
-const Client = ({ username }) => {
+const Client = ({ username, additionalData }) => {
+    // Error handling for missing username
+    if (!username) {
+        return <div className="client">No username provided</div>;
+    }
+
     return (
         <div className="client">
+            {/* Avatar with custom size and roundness */}
             <Avatar name={username} size={50} round="14px" />
+
+            {/* Username display */}
             <span className="userName">{username}</span>
+
+            {/* Additional data rendering if provided */}
+            {additionalData && (
+                <div className="additionalData">
+                    <h4>Additional Data</h4>
+                    <p>{additionalData}</p>
+                </div>
+            )}
         </div>
     );
 };
